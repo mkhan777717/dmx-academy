7e1660b3c3d4ae5dd03150d824985f628ebd69d4
Abhishek Kumar <ak5931431@gmail.com>
2026-06-19 21:46:16 +0530

    feat: adding the ai viva portal

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ const problemRoutes = require('./routes/problemRoutes');
 const submissionRoutes = require('./routes/submissionRoutes');
 const contestRoutes = require('./routes/contestRoutes');
 const livekitRoutes = require('./routes/livekitRoutes');
+const vivaRoutes = require('./routes/vivaRoutes');
 
 
 const app = express();
@@ -47,6 +48,7 @@ app.use('/api/problems', problemRoutes);
 app.use('/api/submissions', submissionRoutes);
 app.use('/api/contests', contestRoutes);
 app.use('/api/livekit', livekitRoutes);
+app.use('/api/viva', vivaRoutes);
 
 // Fallback for undefined routes
 app.use((req, res, next) => {
